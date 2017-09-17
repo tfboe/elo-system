8a7d6a5e41c9e85795da41f536303b94e2d2db13
blacktemplar <blacktemplar@a1.net>
2017-09-17 02:24:20 +0200

    makes Kernel and therefore Artisan accessable again

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -61,6 +61,11 @@ $app->singleton(
   App\Exceptions\Handler::class
 );
 
+$app->singleton(
+  Illuminate\Contracts\Console\Kernel::class,
+  App\Console\Kernel::class
+);
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
