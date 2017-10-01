152a6e029fec8304b1a715f2c51e5c18c5e889e1
blacktemplar <blacktemplar@a1.net>
2017-10-01 17:32:06 +0200

    adds documentation for tournament creation

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ $router->post('login', [
  * @apiVersion 0.1.0
  *
  * @apiHeader {String} Authorization Bearer Authorization Token
- * @apiError UnauthorizedException No token given ot the given token is invalid
+ * @apiError UnauthorizedException No token given or the given token is invalid
  *
  */
 
@@ -76,6 +76,39 @@ $router->group(['middleware' => 'auth:api'], function () use ($router) {
     'as' => 'userId', 'uses' => 'UserController@userId'
   ]);
 
+  /**
+   * @api {post} /createOrUpdateTournament Create or Update a Tournament
+   * @apiUse AuthenticatedRequest
+   * @apiVersion 0.1.0
+   * @apiDescription Creates a new tournament or if the tournament already exists updates it
+   * @apiName PostCreateOrUpdateTournament
+   * @apiGroup Tournament
+   *
+   * @apiParam {String} userIdentifier a identifier which identifies the tournament uniquely across all tournaments of
+   *                                   this user
+   * @apiParam {String} name the name of the tournament
+   * @apiParam {String} [tournamentListId=""] the list id of which the tournament is part of
+   * @apiParam {String=OFFICIAL,SPEEDBALL,CLASSIC} [gameMode] The rule mode of the tournament. All games of the
+   *                                                          tournament which do not specify another game mode will use
+   *                                                          this game mode.
+   * @apiParam {String=ELIMINATION,QUALIFICATION} [organizingMode] The organization mode of the tournament. All games of
+   *                                                               the tournament which do not specify another
+   *                                                               organizing mode will use this organizing mode.
+   * @apiParam {String=ONE_SET,BEST_OF_THREE,BEST_OF_FIVE} [scoreMode] The score mode of the tournament. All games of
+   *                                                                   the tournament which do not specify another
+   *                                                                   score mode will use this score mode.
+   * @apiParam {String=DOUBLE,SINGLE,DYP} [teamMode] Specifies the team mode of the tournament. If the partners were
+   *                                                 chosen randomly at some point the mode should be DYP. All games of
+   *                                                 the tournament which do not specify another table will use this
+   *                                                 table.
+   * @apiParam {String=MULTITABLE,GARLANDO,LEONHART,TORNADO,ROBERTO_SPORT,BONZINI} [table]
+   *           On which sort of table the tournament is played. Multitable should only be used if the table is not known
+   *           anymore or if the game was really a multitable game, i.e. multiple sets on at least two different tables.
+   *           All games of the tournament which do not specify another table will use this table.
+   * @apiError ValidationException The userIdentifier or the name of the tournament are missing or one of the modes or
+   *                               the given table is not in the list of valid options.
+   * @apiSuccess {String} type the type of the successful operation either "create" or "update"
+   */
   $router->post('createOrUpdateTournament', [
     'as' => 'createOrUpdateTournament', 'uses' => 'TournamentController@createOrUpdateTournament'
   ]);
