ad48a7e4393fd2776108c34034f1bb50034c8e4a
blacktemplar <blacktemplar@a1.net>
2018-10-14 21:18:59 +0200

    integer types

--- app/Http/Controllers/AsyncController.php
+++ app/Http/Controllers/AsyncController.php
@@ -30,15 +30,15 @@ class AsyncController
     /** @var AsyncRequest $request */
     $request = $entityManager->find(AsyncRequest::class, $id);
     if ($request === null) {
-      return new JsonResponse(["type" => "0", "message" => "does not exist"]);
+      return new JsonResponse(["type" => 0, "message" => "does not exist"]);
     }
     if ($request->getStartTime() === null) {
-      return new JsonResponse(["type" => "1", "message" => "not yet started"]);
+      return new JsonResponse(["type" => 1, "message" => "not yet started"]);
     }
     if ($request->getEndTime() === null) {
-      return new JsonResponse(["type" => "2", "message" => "running", "progress" => $request->getProgress()]);
+      return new JsonResponse(["type" => 2, "message" => "running", "progress" => $request->getProgress()]);
     }
-    return new JsonResponse(["type" => "3", "message" => "finished", "result" => $request->getResult()]);
+    return new JsonResponse(["type" => 3, "message" => "finished", "result" => $request->getResult()]);
   }
 //</editor-fold desc="Public Methods">
 }
